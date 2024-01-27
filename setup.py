bb32ab6f774718527db58b10060d0d914589131f
VIKALP <113850666+vikalp026var@users.noreply.github.com>
2024-01-27 22:16:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "5.0.0"
+__version__ = "5.0.2"
 REPO_NAME = "databaseauto"
 PKG_NAME= "databaseauto"
 AUTHOR_USER_NAME = "Vikalp"
