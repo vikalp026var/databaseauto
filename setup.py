95a36094d7f33e8784aa523dc185cff17543628d
VIKALP <113850666+vikalp026var@users.noreply.github.com>
2024-01-27 22:22:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 from typing import List
 
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
-   
+    long_description = f.read()
 
 __version__ = "5.0.2"
 REPO_NAME = "databaseauto"
-PKG_NAME= "databaseauto"
+PKG_NAME = "databaseauto"
 AUTHOR_USER_NAME = "Vikalp"
 AUTHOR_EMAIL = "Vikalp026varshney@gmail.com"
 
@@ -16,16 +15,13 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with database.",
+    description="A python package for connecting with a database.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",  # Corrected parameter name
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    )
-
-
-
+)
